1872eaa8d1f95cb0466bdfd25023f244d3b3d8da
anarpi <anarpi@code.edu.az>
2024-06-07 18:14:04 +0400

    new

---
+++ UniversityApi/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using UniversityApi.Data;
+using UniversityApi.Dtos;
+using Services.Services.Implementations;
+using Services.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddDbContext<UniDatabase>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"),
+        sqlOptions => sqlOptions.MigrationsAssembly("UniversityApi"));
+});
+
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateDto>();
+
+builder.Services.AddFluentValidationRulesToSwagger();
+
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IGroupService, GroupService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
